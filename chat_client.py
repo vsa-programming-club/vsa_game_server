42260ca3998fa783fa4cd5d764ebff44e33d8505
davidmerickson01 <44657006+davidmerickson01@users.noreply.github.com>
2018-11-12 23:32:38 -0800

    send/recv msg protocol

--- chat_client.py
+++ chat_client.py
@@ -1,13 +1,32 @@
-import socket, threading
+import socket, threading, struct
 
+def send_one_message(sock, data):
+    length = len(data)
+    sock.sendall(struct.pack('!I', length))
+    sock.sendall(data)
+    
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+    
+def recv_one_message(sock):
+    lengthbuf = recvall(sock, 4)
+    length, = struct.unpack('!I', lengthbuf)
+    return recvall(sock, length)
+    
 def send():
     while True:
         msg = input('CHAT> ')
-        client_sock.send(bytes(chattarget+":CHAT:"+msg, 'utf-8'))
+        send_one_message(client_sock, bytes(chattarget+":CHAT:"+msg, 'utf-8'))
 
 def receive():
     while True:
-        msg = client_sock.recv(1024).decode("utf-8")
+        msg = recv_one_message(client_sock).decode("utf-8")
         print(msg)
 
 host = input('Server address:')
@@ -19,9 +38,9 @@ chattarget = input('Who do you want to chat with? (username or "ALL")')
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_sock.connect((bytes(host, 'utf-8'), port))     
 print('Connected to remote host...')
-client_sock.send(bytes(username, 'utf-8'))
-client_sock.send(bytes(password, 'utf-8'))
-client_sock.send(bytes('SYS:REG:'+chattarget+':CHAT', 'utf-8'))
+send_one_message(client_sock, bytes(username, 'utf-8'))
+send_one_message(client_sock, bytes(password, 'utf-8'))
+send_one_message(client_sock, bytes('SYS:REG:'+chattarget+':CHAT', 'utf-8'))
 
 thread_send = threading.Thread(target = send)
 thread_send.start()
